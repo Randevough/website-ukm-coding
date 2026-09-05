1de2c8e2e2a4c9aafff0f7bcac8e56966acbba19
Rand <randdevs54@gmail.com>
2026-09-05 13:38:41 +0700

    style: format files with prettier

--- sanity/schemaTypes/project.ts
+++ sanity/schemaTypes/project.ts
@@ -103,7 +103,9 @@ export const project = defineType({
       fieldset: "details",
       description: "Nama divisi atau anggota kunci yang mengerjakan.",
       validation: (Rule) =>
-        Rule.max(45).warning("Disarankan maksimal 45 karakter agar pas di header."),
+        Rule.max(45).warning(
+          "Disarankan maksimal 45 karakter agar pas di header.",
+        ),
     }),
     defineField({
       name: "cover",
